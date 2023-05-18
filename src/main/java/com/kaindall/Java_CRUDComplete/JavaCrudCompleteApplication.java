a17f7942adae003a51e2c68bb9a127bb74594ce6
Kaindall <gustavozanete2@gmail.com>
2023-05-18 16:51:30 -0300

    it's the initializr generated by springboot website with some modifications in application.properties to add some content

---
+++ src/main/java/com/kaindall/Java_CRUDComplete/JavaCrudCompleteApplication.java
@@ -0,0 +1,13 @@
+																																																						package com.kaindall.Java_CRUDComplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudCompleteApplication.class, args);
+	}
+
+}
